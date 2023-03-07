848f8c396f1eed287599e4e2dc324842e3878aff
NoHamster <pfoch.tomke@gmail.com>
2023-03-07 15:53:11 +0100

    modified:   lua/_nvim-treesitter.lua 	modified:   lua/plugins.lua

--- lua/_nvim-treesitter.lua
+++ lua/_nvim-treesitter.lua
@@ -7,7 +7,7 @@ return {
 	config = function()
 		require'nvim-treesitter.configs'.setup {
 			-- A list of parser names, or "all" (the four listed parsers should always be installed)
-			ensure_installed = { "c", "cpp", "java", "rust", "json", "vim", 
+			ensure_installed = { "c", "cpp", "rust", "json",
 					     --"help", "bash", "arduino", "css", "gitignore", "html", "javascript", "make", "markdown", "yaml", "toml", "python", "lua" -- known to crash windows
 		},
 
